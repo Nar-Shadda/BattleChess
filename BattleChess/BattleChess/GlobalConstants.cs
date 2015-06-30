4e2d9a0d1b0a4218004af30ab73a86a84830e0f5
darighteous1 <tonislav.atanasov@gmail.com>
2015-06-30 15:34:04 +0300

    added constant library

--- BattleChess/BattleChess/GlobalConstants.cs
+++ BattleChess/BattleChess/GlobalConstants.cs
@@ -7,11 +7,29 @@ namespace BattleChess
 {
     public static class GlobalConstants
     {
-        public const string pawn = "";
-        public const string king = "";
-        public const string queen = "";
-        public const string bishop = "";
-        public const string knight = "";
-        public const string rook = "";
+        public const int WindowWidth = 1280;
+        public const int WindowHeight = 720;
+
+        public const int BoardTopLeftX = 30;
+        public const int BoardTopLeftY = 30;
+
+        public const int BorderTopLeftX = 0;
+        public const int BorderTopLeftY = 0;
+
+        public const string WhitePawn =   "/Sprites/Figures/white_pawn";
+        public const string WhiteKing = "/Sprites/Figures/white_king";
+        public const string WhiteQueen = "/Sprites/Figures/white_queen";
+        public const string WhiteBishop = "/Sprites/Figures/white_bishop";
+        public const string WhiteKnight = "/Sprites/Figures/white_knight";
+        public const string WhiteRook = "/Sprites/Figures/white_rook";
+
+        public const string BlackPawn = "/Sprites/Figures/black_pawn";
+        public const string BlackKing = "/Sprites/Figures/black_king";
+        public const string BlackQueen = "/Sprites/Figures/black_queen";
+        public const string BlackBishop = "/Sprites/Figures/black_bishop";
+        public const string BlackKnight = "/Sprites/Figures/black_knight";
+        public const string BlackRook = "/Sprites/Figures/black_rook";
+
+       
     }
 }
