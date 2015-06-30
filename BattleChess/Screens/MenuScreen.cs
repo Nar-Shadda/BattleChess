63beaaee16cec5e2ba921562134a11d970f656e3
darighteous1 <tonislav.atanasov@gmail.com>
2015-06-30 21:15:47 +0300

    added ALMOST working screen manager

---
+++ BattleChess/Screens/MenuScreen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BattleChess.Screens;
+
+using BattleChess;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BattleChess.Screens
+{
+    class MenuScreen : Screen
+    {
+        
+        public MenuScreen(Game engine) : base(engine)
+        {
+        }
+
+        public override void LoadContent()
+        {
+            //ScreenManager.Instance.Engine.Board
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+         
+        }
+    }
+}
