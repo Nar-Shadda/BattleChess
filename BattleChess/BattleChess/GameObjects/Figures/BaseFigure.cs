6a75b3946fedcce46ed2a13b821970c9f3fd186a
dmaslev <detelinmaslev@gmail.com>
2015-06-30 13:27:18 +0300

    initialize

---
+++ BattleChess/BattleChess/GameObjects/Figures/BaseFigure.cs
@@ -0,0 +1,12 @@
+﻿using BattleChess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BattleChess.GameObjects.Figures
+{
+    class BaseFigure : IFigure
+    {
+    }
+}
