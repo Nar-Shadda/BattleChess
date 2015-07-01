1e1cb1bc65c92ba4e917321a86507266c89371f6
IvanMladenov <ismladenov@gmail.com>
2015-07-01 22:07:48 +0300

    Add exeption class

---
+++ BattleChess/Exceptions/CostomExeption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BattleChess.Exceptions
+{
+    public class CostomExeption : Exception
+    {
+        public CostomExeption(string message)
+            :base(message)
+        {
+        }
+    }
+}
