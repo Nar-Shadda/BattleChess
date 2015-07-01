e09b16d3c9c9a5f26e71d4327922b3bae52aad47
IvanMladenov <ismladenov@gmail.com>
2015-07-01 22:05:04 +0300

    struct fixed

--- BattleChess/Position.cs
+++ BattleChess/Position.cs
@@ -7,13 +7,7 @@ namespace BattleChess
 {
     public struct Position
     {
-
-<<<<<<< HEAD
-        public Position(char col, char row)
-            : this()
-=======
         public Position(char col, char row) : this()
->>>>>>> dcd4ab7c9902ae54e7225a5d672899425e0dc5cb
         {
             this.Row = row;
             this.Col = col;
